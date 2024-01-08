82faffc860ab3ad5c479579a982b03e2b1b5c4dd
Woohyeok Choi <keltpower0@gmail.com>
2024-01-08 15:30:31 +0900

    1.0.0

--- gatsby-node.mjs
+++ gatsby-node.mjs
@@ -1,9 +1,7 @@
 import path from 'path'
-import slugify from "slugify";
-import moment from 'moment'
 import readingTime from 'reading-time'
 
-const TYPES = ['lectures', 'news', 'people', 'projects', 'publications', 'notice.md']
+const TYPES = ['lectures', 'news', 'people', 'projects', 'publications', 'notice']
 
 const RE_TYPES = TYPES.reduce((res, item) => {
     res[item]  = RegExp(`/posts/${item}`)
@@ -14,34 +12,11 @@ export const onCreateNode = async ({ node, actions}) => {
     const { createNodeField } = actions
     if (node.internal.type === 'Mdx') {
         const absolutePath = node.internal.contentFilePath || ''
-        const meta = node.frontmatter || {}
         const type = TYPES.find((type) => RE_TYPES[type].test(absolutePath))
-        let slug = ''
-        switch (type) {
-            case 'lectures':
-                slug = `${meta.year}/${meta.semester}/${meta.division}/${slugify(meta.title, {remove: /[*+~.()'"!:@]/g})}`
-                break
-            case 'news':
-                slug = `${moment(meta.dateTime, 'YYYY-MM-DD kk:mm:ss').format('YYYY-MM-DD')}/${slugify(meta.title, {remove: /[*+~.()'"!:@]/g})}`
-                break
-            case 'people':
-                slug = `${slugify(meta.email)}`
-                break
-            case 'projects':
-                slug = `${moment(meta.startDate).format('YYYY-MM-DD')}/${slugify(meta.title, {remove: /[*+~.()'"!:@]/g})}`
-                break
-            case 'publications':
-                slug = `${moment(meta.date).format('YYYY-MM-DD')}/${slugify(meta.title, {remove: /[*+~.()'"!:@]/g})}`
-                break
-            case 'notice.md':
-                slug = 'notice'
-                break
-        }
-        if (slug) {
-            createNodeField({node: node, name: 'slug', value: slug})
-            if (slug !== 'notice') {
+        if (type) {
+            if (type !== 'notice') {
                 createNodeField({node: node, name: 'postType', value: type})
-                createNodeField({node: node, name: 'sitePath', value: `/${type}/${slug}`})
+                createNodeField({node: node, name: 'sitePath', value: `/${type}/${node.id}`})
             } else {
                 createNodeField({node: node, name: 'postType', value: 'notice'})
                 createNodeField({node: node, name: 'sitePath', value: `/notice`})
